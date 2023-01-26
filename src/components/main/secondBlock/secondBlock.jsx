d60124d44c3c457b1c5d6ab032a2a1aa94a039dd
KoMRaD1240 <komrad.noob@yandex.ru>
2023-01-26 11:28:20 +0300

    123

--- src/components/main/secondBlock/secondBlock.jsx
+++ src/components/main/secondBlock/secondBlock.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
-import styles from "./secondBlock.module.scss"
+import React from "react";
+import styles from "./secondBlock.module.scss";
 export const SecondBlock = () => {
   return (
-    <div className={styles.wrapper}>123</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        Погружаюсь в тему и подбираю ключи,
+        <br />а после пишу текст
+      </div>
+      <div className={styles.desk}>
+        <div className={styles.firstcol}>
+          <div className={styles.deskitem}>
+            <div className={styles.deskhead}>Статьи</div>
+            <div className={styles.desktext}>
+              Анализирую и&nbsp;изучаю тему, подготавливаю ключевые слова,
+              прорабатываю текст и&nbsp;корректирую его.
+            </div>
+          </div>
+          <div className={styles.deskitem}>
+            <div className={styles.deskhead}>Креативный копирайтинг</div>
+            <div className={styles.desktext}>
+              Готовлю неординарные тексты под&nbsp;бренд для&nbsp;новых решений задач в&nbsp;продажах и&nbsp;в&nbsp;рекламном привлечении.
+            </div>
+          </div>
+        </div>
+        <div className={styles.deskitem}>
+          <div className={styles.deskhead}></div>
+          <div className={styles.desktext}></div>
+        </div>
+        <div className={styles.deskitem}>
+          <div className={styles.deskhead}></div>
+          <div className={styles.desktext}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
