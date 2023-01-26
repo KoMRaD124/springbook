2e4648e6d2e6374a41a80d2c3b18a737263cf7c0
KoMRaD1240 <komrad.noob@yandex.ru>
2023-01-26 18:10:40 +0300

    start footer

--- src/components/main/footer/Footer.jsx
+++ src/components/main/footer/Footer.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import styles from "./Footer.module.scss"
+import React from "react";
+import styles from "./Footer.module.scss";
+import { logo } from "../../../assets/main";
 
 export const Footer = () => {
   return (
     <div className={styles.wrapper}>
-        <div className={styles.wrapper}></div>
+      <div className={styles.logoCol}>
+        <img className={styles.logoimg} src={logo} alt="logo" />
+        <div className={styles.logoText}>
+          Староста Юлия
+          <br />
+          копирайтер
+        </div>
+      </div>
+      <div className={styles.firstcol}>
+        <div className={styles.colHead}>Связаться со мной</div>
+        <div className={styles.colitem}>springbook.pro@gmail.com</div>
+        <div className={styles.colitem}>скачать резюме</div>
+      </div>
+      <div className={styles.secondcol}>
+        <div className={styles.colHead}>Меню</div>
+        <div className={styles.colitem}>Обо мне</div>
+        <div className={styles.colitem}>Мои проекты</div>
+      </div>
     </div>
-  )
-}
+  );
+};
